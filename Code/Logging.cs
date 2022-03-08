50ce9217bf060e517e415eea8f132ef49bee0b8a
algernon-A <aeneas@aloku.net>
2022-03-08 15:18:43 +1100

    Reduce logging StringBuilder allocations.

--- Code/Logging.cs
+++ Code/Logging.cs
@@ -13,6 +13,9 @@ namespace RealPop2
         // Logging detail flag.
         internal static bool detailLogging = false;
 
+        // Stringbuilder for messaging.
+        private static StringBuilder message = new StringBuilder(128);
+
 
         /// <summary>
         /// Prints a single-line debugging message to the Unity output log with an "ERROR: " prefix, regardless of the 'detailed logging' setting.
@@ -50,7 +53,8 @@ namespace RealPop2
         {
             // Use StringBuilder for efficiency since we're doing a lot of manipulation here.
             // Start with mod name (to easily identify relevant messages), followed by colon to indicate start of actual message.
-            StringBuilder message = new StringBuilder(RealPopMod.ModName);
+            message.Length = 0;
+            message.Append(RealPopMod.ModName);
             message.Append(": ");
 
             // Add each message parameter.
@@ -89,7 +93,8 @@ namespace RealPop2
         {
             // Use StringBuilder for efficiency since we're doing a lot of manipulation here.
             // Start with mod name (to easily identify relevant messages), followed by colon to indicate start of actual message.
-            StringBuilder message = new StringBuilder(RealPopMod.ModName);
+            message.Length = 0;
+            message.Append(RealPopMod.ModName);
             message.Append(": ");
 
             // Append prefix.
